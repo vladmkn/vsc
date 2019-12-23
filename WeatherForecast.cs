d91aae374ef4b96ee13171f794a0cf0f43d8b889
vladm <vladm@nniirt.ru>
2019-12-23 15:36:32 +0300

    [Required]

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System;
 
 namespace testwapi5
@@ -6,6 +7,7 @@ namespace testwapi5
     {
         public DateTime Date { get; set; }
 
+        [Required]
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
